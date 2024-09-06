6d365f966a34e15db9fd6313a550566550e8072e
VishnuMohanS <vishnumohans62002@gmail.com>
2024-09-06 05:24:15 +0000

    day3

--- project2/src/components/HeroCarousel/HeroCarousel.Component.jsx
+++ project2/src/components/HeroCarousel/HeroCarousel.Component.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
+import HeroSlider from "react-slick"; 
 
 const HeroCarousel = () => {
-  return (
-    <div>HeroCarousel</div>
-  )
-}
+  const [images, setImages] = useState([
+    {
+      adult: false,
+      backdrop_path: "/l0cJd3Oq9gquMJPbQHTOdiOAhM5.jpg",
+      genre_ids: [35, 14],
+      id: 453395,
+      original_language: "en",
+      original_title: "Barbie",
+      overview:
+        "Barbie suffers a crisis that leads her to question her world and her existence.",
+      popularity: 856.125,
+      poster_path: "/s0YjNiOwbxIfByyLBR7xl5Qrxza.jpg",
+      release_date: "2023-07-21",
+      title: "Barbie",
+      video: false,
+      vote_average: 7.4,
+      vote_count: 420,
+    },
+    {
+      adult: false,
+      backdrop_path: "/rqgeBNWXas1yrAyXxwi9CahfScx.jpg",
+      genre_ids: [53],
+      id: 985939,
+      original_language: "en",
+      original_title: "Fall",
+      overview:
+        "For best friends Becky and Hunter, life is all about conquering fears and pushing limits",
+      popularity: 6731.327,
+      poster_path: "/9f5sIJEgyUpFv0ozfA6TurG4j22.jpg",
+      release_date: "2022-08-11",
+      title: "Fall",
+      video: false,
+      vote_average: 7.4,
+      vote_count: 346,
+    },
+  ]);
 
-export default HeroCarousel
+  const settings = {
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    cssEase: "linear",
+  };
+
+  return (
+    <>
+      <div className="lg:hidden">
+        <HeroSlider {...settings}>
+          {images.map((image) => (
+            <div key={image.id} className="w-full h-56 md:h-80 py-3">
+              <img
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+                alt={image.title}
+                className="w-full h-full rounded-md object-cover"
+              />
+            </div>
+          ))}
+        </HeroSlider>
+      </div>
+      <div className="hidden lg:block">
+        <HeroSlider {...settings}>
+          {images.map((image) => (
+            <div key={image.id} className="w-full h-96 px-2 py-3">
+              <img
+                src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+                alt={image.title}
+                className="w-full h-full rounded-md object-cover"
+              />
+            </div>
+          ))}
+        </HeroSlider>
+      </div>
+    </>
+  );
+};
+
+export default HeroCarousel;
--- project2/src/pages/Home.Page.jsx
+++ project2/src/pages/Home.Page.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import DefaultLayoutHoc from '../layout/Default.Layout';
 
 
- import EntertaimentCardSlider from '../components/Entertaiment/EntertaimentCard.Component'
+import EntertaimentCardSlider from '../components/Entertaiment/EntertaimentCard.Component'
 import HeroCarousel from '../components/HeroCarousel/HeroCarousel.Component';
 import PosterSlider from '../components/PosterSlider/PosterSlider.Component';
 
@@ -14,9 +14,51 @@ const HomePage = () => {
 
   return (
     <>
-    <HeroCarousel/>
+      <HeroCarousel />
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
+          The Best of Entertaiment
+        </h1>
+        <EntertaimentCardSlider />
+      </div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <PosterSlider
+          title="Recommended Movies"
+          subtitle="List of recommended Movies"
+          posters={recommendedMovies}
+          isDark={false}
+        />
+      </div>
+
+      <div className="bg-premier-800 py-12">
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <div className="hidden md:flex">
+            <img
+              src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+              className="w-full h-full"
+              alt="RuPay"
+            />
+          </div>
+          <PosterSlider
+            title="Premiers"
+            subtitle="Brand new releases every friday"
+            posters={premierMovies}
+            isDark={true}
+          />
+        </div>
+      </div>
+      <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+        <PosterSlider
+          title="Online Streaming Events"
+          subtitle="Online Streaming Events"
+          posters={onlineStreamEvents}
+          isDark={false}
+        />
+      </div>
     </>
-  )
+  );
 }
 
 export default DefaultLayoutHoc(HomePage);
